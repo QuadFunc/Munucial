8e48257f736fbb1a0a5d79b8179034ea6e753217
Andrew <jiganyg@gmail.com>
2017-07-08 18:59:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var Feed = require('rss-to-json');
+var express = require('express');
+var app = express();
+var jsonParser = require('body-parser').json;
+var rssparser = require('xml2json');
+var rssjson;
 
+app.use(jsonParser());
+
+app.post('/api/v1/rss/json', function(req, res) {
+	if (req.body.rssurl != undefined) {
+		var rssurl = req.body.rssurl;
+		Feed.load(rssurl, function(err, rss) {
+			rssjson = rss;
+		}
+	} else {
+		var rssdata = req.body.rssdata;
+		rssjson = rssparser.toJson(rssdata);
+    }
+	res.json(rssjson);
+}
